f151d59781d4323bdc06534b8068f617770407b1
Alexey Khokhlov <alexeyxo@gmail.com>
2024-07-11 12:21:09 +0300

    fix lint

--- example/main.go
+++ example/main.go
@@ -11,30 +11,6 @@ import (
 )
 
 func main() {
-	//dataSource := pipelaner.DataSource{
-	//	Generators: pipelaner.Generators{
-	//		"exec":      &generator.Exec{},
-	//		"pipelaner": &generator.Pipelaner{},
-	//		// For Test
-	//		"int":  &tests.IntGenerator{},
-	//		"int2": &tests.IntTwoGenerator{},
-	//		"rand": &tests.MapGenerator{},
-	//	},
-	//	Maps: pipelaner.Maps{
-	//		"filter":     &transform.Filter{},
-	//		"debounce":   &transform.Debounce{},
-	//		"throttling": &transform.Throttling{},
-	//		"batch":      &transform.Batch{},
-	//		"chunks":     &transform.Chunk{},
-	//		// For Test
-	//		"int_tr":   &tests.IntTransform{},
-	//		"int_tr_e": &tests.IntTransformEmpty{},
-	//	},
-	//	Sinks: pipelaner.Sinks{
-	//		"console":   sink.NewConsole(pipelaner.NewLogger()),
-	//		"pipelaner": &sink.Pipelaner{},
-	//	},
-	//}
 	agent, err := pipelaner.NewAgent(
 		"example/pipeline.toml",
 	)
